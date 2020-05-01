3fa309c6423ff1791966e294f2904643d65af9c7
Sebastian <sebatorin@hotmail.com>
2020-05-01 13:08:06 +0200

    first impl of g2a web scrapping

--- botdiscord.py
+++ botdiscord.py
@@ -2,6 +2,7 @@ import discord
 import random
 from debounce import debounce
 from BotBE import BotBE
+import asyncio
 #print(discord.__version__)  # check to make sure at least once you're on the right version!
 
 token = open("token.txt", "r").read()
@@ -35,8 +36,8 @@ async def handleQuoteSave(message):
     ack = bot_be.save_quote(quote)
     await general_text_chat.send(ack)
 
-async def handleCustomMessage(message):
-    if len(message.content.split("quieres")) > 2:
+async def handleCustomMessage(message): #TODO: refactor to a better approach
+    if len(message.split(" ")) >= 2:
         options = ["si", "no", "tal vez", "quizas"]
         random_idx = random.randint(0, len(options) - 1)
         await general_text_chat.send(f"{options[random_idx]}")
@@ -47,6 +48,8 @@ async def handleCustomMessage(message):
 async def on_ready():  # method expected by client. This runs once when connected
     print(f'We have logged in as {client.user}')  # notification of login.
     global general_text_chat
+    global daxo_guild
+    daxo_guild = client.get_guild(451813158290587649)
     for channel in client.get_all_channels():
         if str(channel) == "chat":
             general_text_chat = channel
@@ -67,24 +70,56 @@ async def on_message(message):  # event that happens per any message.
     elif the_message.startswith("-dailyquote"):
         await handleQuoteSave(message)
     elif "quieres" in the_message:
-        await handleCustomMessage(message)
+        await handleCustomMessage(the_message)
     elif the_message.startswith("--help"):
-        await general_text_chat.send("-subscribe [tag]")
-        await general_text_chat.send("-unsubscribe [tag]")
-        await general_text_chat.send("-dailyquote [quote diario]")
+        await general_text_chat.send(f"```-subscribe [tag] \n-unsubscribe [tag] \n-dailyquote [quote diario]```")
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    print(f'{member} ---------    {after}')
+    print(f'{member} ---------    {before} {after}')
     global general_text_chat
-    if after.channel != None:
-        #message = bot_be.retrieve_subscribers_from_subscribee(str(member.id))
+    if isVoiceStateValid(before, after):
         members_to_notify = bot_be.retrieve_subscribers_from_subscribee(str(member.id))
         for member_id in members_to_notify:
             a_member = await client.fetch_user(member_id)
             if a_member != None:
                 dm_channel = await a_member.create_dm()
-                await dm_channel.send(f"{member.display_name} ha entrado al discord!!!!!!!!!!!")
+                await dm_channel.send(f"{member.display_name} ha entrado al canal {after.channel.name}")
 
+def isVoiceStateValid(before, after):
+    return after.channel != None and not before.self_deaf and not before.self_mute and not before.self_stream and not after.self_deaf and not after.self_mute and not after.self_stream
 
+async def showServerStats():
+    await client.wait_until_ready()
+    global daxo_guild
+    global general_text_chat
+    
+    while not client.is_closed():
+        try:
+            in_activity, online, idle, offline = getReport(daxo_guild)
+            await general_text_chat.send(f"```Online: {online} huevones.\nHaciendo ni mierda: {idle} huevones.\nJugando algo: {in_activity} huevones.\nOffline: {offline} huevones```")
+            await asyncio.sleep(3600)
+        except Exception as e:
+            print(str(e) + " but no problem")
+            await asyncio.sleep(5)
+
+def getReport(guild):
+    online = 0
+    idle = 0
+    offline = 0
+    in_activity = 0
+    for m in guild.members:
+        if len(m.activities) > 0:
+            for activity in m.activities:
+                if str(activity.type) == "playing":
+                    in_activity += 1 
+        elif len(m.activities) == 0 and str(m.status) != "offline":
+            idle += 1
+        if str(m.status) == "online":
+            online += 1
+        if str(m.status) == "offline":
+            offline += 1
+    return in_activity, online, idle, offline
+
+client.loop.create_task(showServerStats())
 client.run(token)  # recall my token was saved!
